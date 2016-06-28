9f732c71046d74ee3a4a0044486c4abf958110be
Patrice Chalin <pchalin@gmail.com>
2016-06-28 16:55:54 -0700

    Sync with 8d7e7a0
    Synced with angular/angular.io master branch, commit 8d7e7a0:
    https://github.com/angular/angular.io/tree/8d7e7a01878d1f8761539948421431fd61b5b51b/public/docs/_examples/toh-6/dart

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,7 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/platform/browser.dart';
-import 'package:http/http.dart';
 import 'package:angular2_tour_of_heroes/app_component.dart';
+import 'package:http/http.dart';
 import 'package:angular2_tour_of_heroes/in_memory_data_service.dart';
 
 void main() {
@@ -9,8 +9,14 @@ void main() {
       const [const Provider(Client, useClass: InMemoryDataService)]);
 }
 /*
+import 'package:http/browser_client.dart';
 
 void main() {
-  bootstrap(AppComponent, const [BrowserClient]);
+  bootstrap(AppComponent, [
+    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
+  ]);
+  // Simplify bootstrap provider list to [BrowserClient]
+  // once there is a fix for:
+  // https://github.com/angular/angular/issues/9673
 }
 */
