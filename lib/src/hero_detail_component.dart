6d7bf2102dd7a5f6496790982cd10a416f16a2a8
Patrice Chalin <pchalin@gmail.com>
2017-08-17 15:33:33 -0400

    Sync with 7b91fb7
    Synced with dart-lang/site-webdev 4-dev branch, commit 7b91fb7:
    https://github.com/dart-lang/site-webdev/tree/7b91fb78decd00b545ec12081a6922442cd496a7/examples/ng/doc/toh-6

--- lib/src/hero_detail_component.dart
+++ lib/src/hero_detail_component.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_router/angular_router.dart';
 
 import 'hero.dart';
@@ -10,7 +11,7 @@ import 'hero_service.dart';
   selector: 'hero-detail',
   templateUrl: 'hero_detail_component.html',
   styleUrls: const ['hero_detail_component.css'],
-  directives: const [COMMON_DIRECTIVES],
+  directives: const [CORE_DIRECTIVES, formDirectives],
 )
 class HeroDetailComponent implements OnInit {
   Hero hero;
