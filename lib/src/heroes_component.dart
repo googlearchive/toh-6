eedd23e01574d3815764947c12350608d723a264
Patrice Chalin <pchalin@gmail.com>
2017-10-07 07:43:26 -0400

    Sync with 40ab65a
    Synced with dart-lang/site-webdev master branch, commit 40ab65a:
    https://github.com/dart-lang/site-webdev/tree/40ab65aed3a4162d7be25dd4c35b991370a1b0fb/examples/ng/doc/toh-6

--- lib/src/heroes_component.dart
+++ lib/src/heroes_component.dart
@@ -15,11 +15,10 @@ import 'hero_service.dart';
   pipes: const [COMMON_PIPES],
 )
 class HeroesComponent implements OnInit {
-  List<Hero> heroes;
-  Hero selectedHero;
-
   final HeroService _heroService;
   final Router _router;
+  List<Hero> heroes;
+  Hero selectedHero;
 
   HeroesComponent(this._heroService, this._router);
 
@@ -40,13 +39,9 @@ class HeroesComponent implements OnInit {
     if (selectedHero == hero) selectedHero = null;
   }
 
-  void ngOnInit() {
-    getHeroes();
-  }
+  void ngOnInit() => getHeroes();
 
-  void onSelect(Hero hero) {
-    selectedHero = hero;
-  }
+  void onSelect(Hero hero) => selectedHero = hero;
 
   Future<Null> gotoDetail() => _router.navigate([
         'HeroDetail',
